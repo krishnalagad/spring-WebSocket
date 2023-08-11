47cfafad9786958c6f673b5281d0546974e3e841
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-11 14:38:14 +0530

    - dump DTOs and Controller for web socket.

---
+++ websocket/src/main/java/com/learnspring/websocket/controller/MessageController.java
@@ -0,0 +1,23 @@
+package com.learnspring.websocket.controller;
+
+import com.learnspring.websocket.dto.Message;
+import com.learnspring.websocket.dto.ResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class MessageController {
+
+    @MessageMapping("/message")
+    @SendTo("/topic/messages")
+    public ResponseEntity<ResponseMessage> getMessage(final Message message) throws InterruptedException {
+
+        Thread.sleep(1000);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(
+                HtmlUtils.htmlEscape(message.getMessageContent())));
+    }
+}

a0c927d89a88a6712262ae76e40c230c678cf69f
Krishna Lagad <krishnalagad2@gmail.com>
2023-08-14 19:38:38 +0530

    - dump apiinfo

--- websocket/src/main/java/com/learnspring/websocket/WebsocketApplication.java
+++ websocket/src/main/java/com/learnspring/websocket/WebsocketApplication.java
@@ -1,9 +1,29 @@
 package com.learnspring.websocket;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring WebSocket",
+				version = "1.0.0",
+				description = "This is demo application that demonstrates Spring Websocket",
+				contact = @Contact(
+						name = "Dev. Krishna Lagad",
+						email = "support@gmail.com"
+				),
+				license = @License(
+						name = "KrishnaL",
+						url = "http://bit.ly/3x2D8oj"
+				)
+
+		)
+)
 public class WebsocketApplication {
 
 	public static void main(String[] args) {
--- websocket/src/main/java/com/learnspring/websocket/controller/MessageRestController.java
+++ websocket/src/main/java/com/learnspring/websocket/controller/MessageRestController.java
@@ -2,6 +2,7 @@ package com.learnspring.websocket.controller;
 
 import com.learnspring.websocket.dto.Message;
 import com.learnspring.websocket.service.WebSocketService;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +24,15 @@ public class MessageRestController {
     private Logger logger = LoggerFactory.getLogger(MessageRestController.class);
 
     @PostMapping("/send-message")
+    @Hidden
     public ResponseEntity<?> sendMessage(Message message) {
         this.webSocketService.notifyToFrontend(message);
         return ResponseEntity.status(HttpStatus.OK).body(message);
+    }
+
+    @GetMapping("/")
+    public String testApi() {
+        return "This is testing API";
     }
 
     @MessageMapping("/message")
